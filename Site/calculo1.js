e1756200c104cd6058d45fa511312351d824e123
MarcosVinicius1996 <115051272+MarcosVinicius1996@users.noreply.github.com>
2022-11-21 14:29:52 -0300

    Delete calculo1.js

--- Site/calculo1.js
+++
@@ -1,8 +0,0 @@
-var distancia, km
-
-function divideBy(){
-    distancia= parseFloat(frdados.txdistancia.value.replace(",","."));
-    km= parseFloat(frdados.txquilometros.value.replace(",","."));
-    document.getElementById("result").innerHTML = distancia / km;
-    document.getElementById("imagem2").src="imagens/viagem.png";
-}
